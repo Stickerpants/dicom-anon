19b8c4548e27a70381ca36d2fcee896863054820
Tyler Sarris <tyler.sarris@gmail.com>
2016-03-28 13:52:05 -0700

    a class needed for the lookup method

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/dictItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dicom;
+
+namespace WindowsFormsApplication1
+{
+    public class dictItem
+    {
+        public DicomTag tag;
+        public string vr;
+        public string vm;
+        public string description;
+    }
+}

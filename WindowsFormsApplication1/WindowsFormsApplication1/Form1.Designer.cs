e6317435e4870e87278adecb309eef3e4a64d9e8
danaklamut <danak@shaw.ca>
2016-03-29 19:51:24 -0700

    Updated code -used for Sprint 2 demo

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -1,126 +1,121 @@
-﻿namespace WindowsFormsApplication1
+namespace WindowsFormsApplication1
 {
-    partial class Form1
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        partial class Form1
         {
-            if (disposing && (components != null))
+            /// <summary>
+            /// Required designer variable.
+            /// </summary>
+            private System.ComponentModel.IContainer components = null;
+            /// <summary>
+            /// Clean up any resources being used.
+            /// </summary>
+            /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+            protected override void Dispose(bool disposing)
             {
-                components.Dispose();
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
             }
-            base.Dispose(disposing);
+            #region Windows Form Designer generated code
+            /// <summary>
+            /// Required method for Designer support - do not modify
+            /// the contents of this method with the code editor.
+            /// </summary>
+            private void InitializeComponent()
+            {
+                this.textBox1 = new System.Windows.Forms.TextBox();
+                this.button1 = new System.Windows.Forms.Button();
+                this.pictureBox1 = new System.Windows.Forms.PictureBox();
+                this.button2 = new System.Windows.Forms.Button();
+                this.button3 = new System.Windows.Forms.Button();
+                this.button4 = new System.Windows.Forms.Button();
+                ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+                this.SuspendLayout();
+                // 
+                // textBox1
+                // 
+                this.textBox1.Location = new System.Drawing.Point(200, 599);
+                this.textBox1.Name = "textBox1";
+                this.textBox1.Size = new System.Drawing.Size(612, 26);
+                this.textBox1.TabIndex = 0;
+                // 
+                // button1
+                // 
+                this.button1.Location = new System.Drawing.Point(912, 594);
+                this.button1.Name = "button1";
+                this.button1.Size = new System.Drawing.Size(175, 37);
+                this.button1.TabIndex = 1;
+                this.button1.Text = "Load";
+                this.button1.UseVisualStyleBackColor = true;
+                this.button1.Click += new System.EventHandler(this.button1_Click);
+                //this.button1.Label namelabel = new Label();
+                //namelabel.Location = new Point(13, 13);
+               // this.button1.Click += new System.Windows.Forms.TextBox(); ;
+                //this.Controls.Add(namelabel);
+                // 
+                // pictureBox1
+                // 
+                this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+                this.pictureBox1.Name = "pictureBox1";
+                this.pictureBox1.Size = new System.Drawing.Size(1002, 576);
+                this.pictureBox1.TabIndex = 2;
+                this.pictureBox1.TabStop = false;
+                // 
+                // button2
+                // 
+                this.button2.Location = new System.Drawing.Point(818, 594);
+                this.button2.Name = "button2";
+                this.button2.Size = new System.Drawing.Size(88, 37);
+                this.button2.TabIndex = 3;
+                this.button2.Text = "Browse";
+                this.button2.UseVisualStyleBackColor = true;
+                this.button2.Click += new System.EventHandler(this.button2_Click);
+                // 
+                // button3
+                // 
+                this.button3.Location = new System.Drawing.Point(106, 594);
+                this.button3.Name = "button3";
+                this.button3.Size = new System.Drawing.Size(88, 37);
+                this.button3.TabIndex = 4;
+                this.button3.Text = "Next";
+                this.button3.UseVisualStyleBackColor = true;
+                this.button3.Click += new System.EventHandler(this.button3_Click);
+                // 
+                // button4
+                // 
+                this.button4.Location = new System.Drawing.Point(12, 594);
+                this.button4.Name = "button4";
+                this.button4.Size = new System.Drawing.Size(88, 37);
+                this.button4.TabIndex = 5;
+                this.button4.Text = "Previous";
+                this.button4.UseVisualStyleBackColor = true;
+                this.button4.Click += new System.EventHandler(this.button4_Click);
+                // 
+                // Form1
+                // 
+                this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                this.ClientSize = new System.Drawing.Size(1026, 643);
+                this.Controls.Add(this.button4);
+                this.Controls.Add(this.button3);
+                this.Controls.Add(this.button2);
+                this.Controls.Add(this.pictureBox1);
+                this.Controls.Add(this.button1);
+                this.Controls.Add(this.textBox1);
+                this.Name = "Form1";
+                this.Text = "Form1";
+                ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+                this.ResumeLayout(false);
+                this.PerformLayout();
+            }
+            #endregion
+            private System.Windows.Forms.TextBox textBox1;
+            private System.Windows.Forms.Button button1;
+            private System.Windows.Forms.PictureBox pictureBox1;
+            private System.Windows.Forms.Button button2;
+            private System.Windows.Forms.Button button3;
+            private System.Windows.Forms.Button button4;
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.Input = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.tabPage5 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.Input);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Controls.Add(this.tabPage4);
-            this.tabControl1.Controls.Add(this.tabPage5);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1064, 602);
-            this.tabControl1.TabIndex = 0;
-            // 
-            // Input
-            // 
-            this.Input.Location = new System.Drawing.Point(4, 29);
-            this.Input.Name = "Input";
-            this.Input.Padding = new System.Windows.Forms.Padding(3);
-            this.Input.Size = new System.Drawing.Size(1056, 569);
-            this.Input.TabIndex = 0;
-            this.Input.Text = "Input";
-            this.Input.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1056, 569);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Anonymize";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 29);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(1056, 569);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Listen";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // tabPage4
-            // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 29);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(1056, 569);
-            this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Send";
-            this.tabPage4.UseVisualStyleBackColor = true;
-            // 
-            // tabPage5
-            // 
-            this.tabPage5.Location = new System.Drawing.Point(4, 29);
-            this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(1056, 569);
-            this.tabPage5.TabIndex = 4;
-            this.tabPage5.Text = "View";
-            this.tabPage5.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1088, 626);
-            this.Controls.Add(this.tabControl1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
-            this.ResumeLayout(false);
-
-        }
-
-        #endregion
-
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage Input;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.TabPage tabPage4;
-        private System.Windows.Forms.TabPage tabPage5;
-    }
 }
-
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -1,77 +1,70 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Dicom;
-using Dicom.Imaging;
-
+//using Dicom;
+//using Dicom.Imaging;
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
         string[] fileEntries;
         int index;
-        
         public Form1()
         {
             InitializeComponent();
             index = 0;
         }
-        
-         /**
-        *Loads DICOM file into form to manipulate
-        */
+        /**
+       *Loads DICOM file into form to manipulate
+       */
         private void button1_Click(object sender, EventArgs e)
         {
-
-           fileEntries = Directory.GetFiles(@"" + this.textBox1.Text);
-            
-           var image = new DicomImage(@""+fileEntries.GetValue(0).ToString());
-
-           this.pictureBox1.Image = image.RenderImage().AsBitmap();
-             
-
+            //fileEntries = Directory.GetFiles(this.result.Text);
+           // var image = new Image(@"" + fileEntries.GetValue(0).ToString());
+          //  index = 0;
+            button1.Text = "Upload Successful";
+            Label namelabel = new Label();
+            namelabel.Location = new Point(13, 13);
+            namelabel.Text = "Upload Successful";
+            this.Controls.Add(namelabel);
+          //  this.pictureBox1.Image = image.RenderImage().AsBitmap();
+          //  string result = "Upload Successful";
+          //  Label.Text = result.ToString();
         }
-
-
         /**
         *method used to browse for DICOM files to load
         */
         private void button2_Click(object sender, EventArgs e)
-        {            
+        {
+            button1.Text = "Load";
             FolderBrowserDialog openFileDialog1 = new FolderBrowserDialog();
             DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
             if (result == DialogResult.OK) // Test result.
             {
                 string file = openFileDialog1.SelectedPath;
                 this.textBox1.Text = file;
-
             }
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
             index++;
             fileEntries = Directory.GetFiles(@"" + this.textBox1.Text);
-
-            var image = new DicomImage(@"" + fileEntries.GetValue(index).ToString());
-
-            this.pictureBox1.Image = image.RenderImage().AsBitmap();
+           // var image = new DicomImage(@"" + fileEntries.GetValue(index).ToString());
+           // this.pictureBox1.Image = image.RenderImage().AsBitmap();
         }
-
         private void button4_Click(object sender, EventArgs e)
         {
             index--;
             fileEntries = Directory.GetFiles(@"" + this.textBox1.Text);
-
-            var image = new DicomImage(@"" + fileEntries.GetValue(index).ToString());
-
-            this.pictureBox1.Image = image.RenderImage().AsBitmap();
+          //  var image = new DicomImage(@"" + fileEntries.GetValue(index).ToString());
+           // this.pictureBox1.Image = image.RenderImage().AsBitmap();
         }
     }
 }
